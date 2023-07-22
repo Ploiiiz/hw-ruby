428810d5e8e09313f1d29494d53ede2c43fb7a6d
Nathamon <110463038+Ploiiiz@users.noreply.github.com>
2023-07-23 06:03:06 +0700

    part 1 & 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each {|n| sum += n}
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  case arr.length
+  when 0
+    return 0
+  when 1
+    arr.each {|n| return n}
+  else
+    sum = arr.sort[-2] + arr.sort[-1]
+    return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  a = Hash.new
+  arr.each do |val|
+    if a.key? val
+      return true
+    
+    else
+      a[n-val] = val    
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  consonant = "bcdfghjklmnpqrstvwsxyz"    
+  if consonant.include? s[0].downcase
+    return true
+  else
+    return false
+  end
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+
+  if s == '0'
+    return true
+  else
+      if s.to_i(2) % 4 == 0 && s.to_i(2) != 0
+          return true
+      else
+          return false
+      end
+  end
 end
 
 # Part 3
